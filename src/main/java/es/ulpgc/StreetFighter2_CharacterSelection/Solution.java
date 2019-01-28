a7221eb147c722bb779eb91c04a371edc6b84c79
vidaxd <>
2019-01-28 20:18:32 +0000

    Solution class refactored

--- src/main/java/es/ulpgc/StreetFighter2_CharacterSelection/Solution.java
+++ src/main/java/es/ulpgc/StreetFighter2_CharacterSelection/Solution.java
@@ -1,49 +1,42 @@
 package es.ulpgc.StreetFighter2_CharacterSelection;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+
+import static java.util.Arrays.*;
 
 public class Solution{
     public static String[] streetFighterSelection(String[][] fighters, int[] position, String[] moves){
         List<String> characters = new ArrayList<>();
         if (moves.length != 0) {
-            for (String move: moves) {
-                switch (move) {
-                    case "up":
-                        if (position[0]!=0) position[0]-=1;
-                        characters.add(fighters[position[0]][position[1]]);
-                        break;
+            stream(moves).forEachOrdered(move -> {
+                if ("up".equals(move)) {
+                    if (position[0] != 0) position[0] -= 1;
+                    characters.add(fighters[position[0]][position[1]]);
 
-                    case "down":
-                        if (position[0]!=1) position[0]+=1;
-                        characters.add(fighters[position[0]][position[1]]);
-                        break;
+                } else if ("down".equals(move)) {
+                    if (position[0] != 1) position[0] += 1;
+                    characters.add(fighters[position[0]][position[1]]);
 
-                    case "left":
-                        if (position[1]==0) {
-                            position[1]=fighters[position[0]].length-1;
-                        } else {
-                            position[1]-=1;
-                        }
-                        characters.add(fighters[position[0]][position[1]]);
-                        break;
+                } else if ("left".equals(move)) {
+                    if (position[1] == 0) {
+                        position[1] = fighters[position[0]].length - 1;
+                    } else {
+                        position[1] -= 1;
+                    }
+                    characters.add(fighters[position[0]][position[1]]);
 
-                    case "right":
-                        if (position[1]==fighters[position[0]].length-1) {
-                            position[1]=0;
-                        } else {
-                            position[1]+=1;
-                        }
-                        characters.add(fighters[position[0]][position[1]]);
-                        break;
+                } else if ("right".equals(move)) {
+                    if (position[1] == fighters[position[0]].length - 1) {
+                        position[1] = 0;
+                    } else {
+                        position[1] += 1;
+                    }
+                    characters.add(fighters[position[0]][position[1]]);
+
                 }
-            }
-            String[] result = new String[characters.size()];
-            for (int i = 0; i < result.length; i++) {
-                result[i] = characters.get(i);
-            }
-            return result;
+            });
+            return characters.stream().toArray(String[]::new);
         }
         return new String[]{};
     }
